cc39479a8a942177b6d6ebd3637c6de3a6a05375
nicks-bro <43015966+NicholusMuwonge@users.noreply.github.com>
2020-03-04 19:27:46 +0300

    Adding question and solution to smallest sum

---
+++ SmallestSum.py
@@ -0,0 +1,43 @@
+In this kata, you must create a digital root function.
+
+A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+Here's how it works:
+
+digital_root(16)
+=> 1 + 6
+=> 7
+
+digital_root(942)
+=> 9 + 4 + 2
+=> 15 ...
+=> 1 + 5
+=> 6
+
+digital_root(132189)
+=> 1 + 3 + 2 + 1 + 8 + 9
+=> 24 ...
+=> 2 + 4
+=> 6
+
+digital_root(493193)
+=> 4 + 9 + 3 + 1 + 9 + 3
+=> 29 ...
+=> 2 + 9
+=> 11 ...
+=> 1 + 1
+=> 2
+
+
+
+def digital_root(n):
+    total=0
+    new_n=list(str(n))
+    for i in new_n:
+         total=0+int(i)
+         while len(str(total))>1:
+            total=digital_root(total)
+            if len(str(total))==1:
+                break
+    return total
+ 
